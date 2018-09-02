56bd099f2c4cc2dfb7e511507c779db4ee43effd
Nitin Patel <nitinpatel278@gmail.com>
2018-09-02 16:15:58 +0530

    removed redux extension config for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -7,10 +7,7 @@ const middleWare = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleWare))
 );
 
 export default store;
